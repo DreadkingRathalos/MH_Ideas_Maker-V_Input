2def28d2648f4c29111a346aceee3e4e07063017
DreadkingRathalos <108301431+DreadkingRathalos@users.noreply.github.com>
2022-07-27 09:05:24 -0500

    Update Locale_Maker.py

--- Locale_Maker.py
+++ Locale_Maker.py
@@ -1,4 +1,4 @@
 import random
 Adjectives = ['Scorching','Crumbling','Rotten','Infested','Torn','Dampened','Colossal','Gloomy','Brilliant','Charged','Volatile',"Dragon's",'Bleak','Glacial','Tropical','Ancient','Plagued','Primordial','Distorted','Bygone','Vanished','Terrible'] 
-Locale = ['Forest','Jungle','Desert','Lowlands','Volcano','Swamp','Ruins','Field','Mountains','Plains','Island','Seaside','Vale','Caverns','Wastes','Lands','Reach','Peaks','Town','Farmland, Lair']
+Locale = ['Forest','Jungle','Desert','Lowlands','Volcano','Swamp','Ruins','Field','Mountains','Plains','Island','Seaside','Vale','Caverns','Wastes','Lands','Reach','Peaks','Town','Farmland', 'Lair']
 print('The New Locale is a ' + random.choice(Adjectives) + ' ' +  random.choice(Locale) + '.')
